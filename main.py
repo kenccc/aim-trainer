9c1f5b871be6d2b63aa7a05141916f1f8cae8617
Ken Vu <kenvu5530@gmail.com>
2023-07-22 16:21:50 +0200

    creation

---
+++ main.py
@@ -0,0 +1,75 @@
+from ursina import *
+import random
+from random import uniform
+
+app = Ursina()
+score = 0
+missed = 0
+
+class Background(Button):
+    def __init__(self):
+        super().__init__(
+            model="quad",
+            parent=scene,
+            color=color.gray,
+            scale=25,
+        )
+
+    def on_click(self):
+        global missed
+        missed = missed + 1
+        self.color=color.gray
+
+    def on_mouse_enter(self):
+        pass  
+
+    def on_mouse_exit(self):
+        pass
+    
+
+
+
+class Box(Button):
+    def __init__(self):
+        super().__init__(
+            model="circle",
+            parent=camera.ui,
+            color=color.green,
+            scale=0.035,
+            position=(0, 0)
+        )
+
+    def on_click(self):
+        global score
+        score = score + 1
+        self.position = (uniform(-.86, .86), uniform(-.47, .47))
+
+    def on_mouse_exit(self):
+        self.color = color.white
+
+bg = Background()
+box = Box()
+
+def display_results():
+    window_panel = WindowPanel(
+        title=f"Game Over\nTotal Score: {score}\nTotal Missed: {missed}",
+        content=f"Total Missed: {missed}\nTotal Score: {score}",
+        model="quad",
+        scale=(0.5, 0.3),
+        background=True,
+        background_color=color.white66,
+        text_origin=(-0.2, 0.05),
+        text_scale=1.5,
+    )
+
+    def close_panel():
+        window_panel.close()
+        application.quit()  
+
+    invoke(close_panel, delay=2) 
+
+def input(key):
+    if key == 'escape':
+        display_results()
+
+app.run()
---
+++ readme.md
@@ -0,0 +1,7 @@
+This game helps you improve you aim in shooter games, it is very simple but also effective.
+In this game you click on various circles that add a point to your score.
+To start the game you have to click on the first circle.
+
+Controls:
+ESC = quit game
+
